1593df557b410cdd681c0e4472f8c3af400e629b
a01658393 <88850947+a01658393@users.noreply.github.com>
2021-08-23 23:37:25 -0500

    Update exercise.py

--- assignments/02AreaTriangulo/src/exercise.py
+++ assignments/02AreaTriangulo/src/exercise.py
@@ -1,7 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
     pass
-
+height=float(input("Give me the value of the height"))
+base=float(input("Give me the value of the base"))
+result=((height*base)/2)
+print("Your result is", result)
 
 
 if __name__ == '__main__':
