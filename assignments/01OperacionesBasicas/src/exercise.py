82c76836ec8983b599f2dbc969443269d08ed143
a01658393 <88850947+a01658393@users.noreply.github.com>
2021-08-23 23:28:50 -0500

    Update exercise.py

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -1,6 +1,15 @@
 def main():
     #escribe tu código abajo de esta línea
     pass
+num1=float(input("Give me the first number"))
+num2=float(input("Give me the second number"))
+multiplication=num1*num2
+addition=num1+num2
+substraction=num1-num2
+print("Your result is")
+print("Ad", addition)
+print("Sub", substraction)
+print("Multi", multiplication)
 
 
 if __name__ == '__main__':
