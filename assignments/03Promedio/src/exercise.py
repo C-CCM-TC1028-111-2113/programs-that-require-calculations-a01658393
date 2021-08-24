74894feb2f0412beb4631291773672fe48989ea9
a01658393 <88850947+a01658393@users.noreply.github.com>
2021-08-23 23:44:26 -0500

    Update exercise.py

--- assignments/03Promedio/src/exercise.py
+++ assignments/03Promedio/src/exercise.py
@@ -1,7 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
     pass
-
+grade1=float(input("Give me grade 1"))
+grade2=float(input("Give me grade 2"))
+grade3=float(input("Give me grade 3"))
+grade4=float(input("Give me grade 4"))
+average=((grade1+grade2+grade3+grade4)/4)
+print("Your average is", average)
 
 if __name__ == '__main__':
     main()
